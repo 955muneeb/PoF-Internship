558ff7bba49714dd01ca6c070104a4a8b1a8a459
Muneeb Ur Rehman <awanzada1289@gmail.com>
2026-08-08 21:15:13 +0500

    Delete W2/student_management/W1 directory

--- W2/student_management/W1/basicss/06-arrays.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$students = ["Ali","Ahmed","Muneeb","Usman"];
-
-echo $students[0];
-
-echo "<br>";
-
-echo $students[2];
--- W2/student_management/W1/basicss/07-functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function greet(){
-
-    echo "Welcome to PHP";
-
-}
-
-greet();
